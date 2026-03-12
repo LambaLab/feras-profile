a529e99b36f2ef2c98e3294d81dd2a0def081e98
Nagi <nagi@Nagi-Mcbook-Pro.local>
2026-03-12 18:01:43 +0400

    feat: add BrowserRouter with / and /humain routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import { profile } from './data/profileData'
+import { profileHumain } from './data/profileDataHumain'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App profile={profile} />} />
+        <Route path="/humain" element={<App profile={profileHumain} />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
